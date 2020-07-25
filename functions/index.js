e605eba11dbc8b9dde6cca2fa195ce3322259083
Sha-Kumar <shashankneeded@gmail.com>
2020-07-25 20:50:38 +0530

    Deleted Generated Code

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,5 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 const admin = require('firebase-admin');
 const express = require('express');
 const cors = require('cors');
