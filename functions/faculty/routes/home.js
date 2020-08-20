f3dc07dc51aa72bbe9adf53c353a892f995f9e36
Sha-Kumar <shashankneeded@gmail.com>
2020-08-20 19:25:17 +0530

    doing home API

--- functions/faculty/routes/home.js
+++ functions/faculty/routes/home.js
@@ -6,6 +6,22 @@ const middleware = require('../auth/middleware');
 
 homeRoute.get('/', async (req, res, next) => {
     try {
+        // const docData = await (await db.collection(req.usertype).doc(req.uid).get()).data();
+        const docData = await (await db.collection(req.usertype).doc(req.uid).get()).data();
+        const postsRef = db.collection('posts');
+        const scopeData = docData.scope;
+        const len = scopeData.length;
+        var resData = [];
+        var objsData;
+        if(len<9){
+            objsData = await postsRef.where('scope','array-contains-any', scopeData);
+        }
+        else{
+            let newScope = scopeData.slice(0,9);
+            newScope.push('global');
+            objsData = await postsRef.where('scope','array-contains-any', newScope);
+        }
+
         return res.status(200).send('All the post data fetched');
     } catch (err) {
         return res.send(err.toString());
