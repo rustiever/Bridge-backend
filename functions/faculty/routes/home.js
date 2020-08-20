7d36f8be16527718b1bed06b1601591e5d3168a8
Sha-Kumar <shashankneeded@gmail.com>
2020-08-20 21:06:44 +0530

    small correction

--- functions/faculty/routes/home.js
+++ functions/faculty/routes/home.js
@@ -7,21 +7,27 @@ const middleware = require('../auth/middleware');
 homeRoute.get('/', async (req, res, next) => {
     try {
         // const docData = await (await db.collection(req.usertype).doc(req.uid).get()).data();
-        const docData = await (await db.collection(req.usertype).doc(req.uid).get()).data();
+        const docData = await (await db.collection('faculties').doc("H3KOr0tq8Wcg5cK8HIY6AeRRZj73").get()).data();
         const postsRef = db.collection('posts');
         const scopeData = docData.scope;
+        //console.log(scopeData);
         const len = scopeData.length;
         var resData = [];
+        var limit = 50;
         var objsData;
         if(len<9){
-            objsData = await postsRef.where('scope','array-contains-any', scopeData);
+            scopeData.push('global');
+            objsData = await postsRef.where('scope','array-contains-any', scopeData).orderBy('timeStamp','asc').limit(50).get();
         }
         else{
             let newScope = scopeData.slice(0,9);
             newScope.push('global');
-            objsData = await postsRef.where('scope','array-contains-any', newScope);
+            objsData = await postsRef.where('scope','array-contains-any', newScope).orderBy('timeStamp', 'asc').limit(50).get();
         }
-
+        objsData.forEach(element => {
+            console.log(element.id);
+            console.log(element.data());
+        });
         return res.status(200).send('All the post data fetched');
     } catch (err) {
         return res.send(err.toString());
