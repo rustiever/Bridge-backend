6068df050db31562cec928cbdb0d1bd0a8b1cbe0
Sha-Kumar <shashankneeded@gmail.com>
2020-08-21 12:20:27 +0530

    Anonymous User API correction

--- functions/routes/publicHome.js
+++ functions/routes/publicHome.js
@@ -13,23 +13,18 @@ anonymousRouter.get('/', async (req, res, next) => {
         var obj = [];
         docsRef.forEach(doc => {
             var d = doc.data();
-            //obj.push(doc.data());
             obj.push({
                 postId : doc.id,
                 caption : d.caption,
                 likes: (d.likes).length,
                 photoUrl: d.photoUrl,
-                //scope: d.scope,
                 ownerName: d.ownerName,
                 ownerPhotoUrl: d.ownerPhotoUrl,
                 ownerUid: d.ownerUid,
                 timeStamp: d.timeStamp.seconds,
-                //For now i kept comments as a number
                 comments:(d.comments).length
             });
         });
-
-
         //Give the object name as feedData in the result...
         result.feedData = obj;
         return res.status(200).json(result);
