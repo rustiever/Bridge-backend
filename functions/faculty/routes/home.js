61a1dbf1de6208d0d5e124da7c31133a4126a8cd
Sha-Kumar <shashankneeded@gmail.com>
2020-08-25 20:43:44 +0530

    Faculty homepage doing

--- functions/faculty/routes/home.js
+++ functions/faculty/routes/home.js
@@ -8,27 +8,35 @@ homeRoute.get('/', async (req, res, next) => {
     try {
         // const docData = await (await db.collection(req.usertype).doc(req.uid).get()).data();
         const docData = await (await db.collection('faculties').doc("H3KOr0tq8Wcg5cK8HIY6AeRRZj73").get()).data();
-        const postsRef = db.collection('posts');
+        // const postsRef = await db.collection('posts').orderBy('timeStamp', 'asc').limit(10).get();
         const scopeData = docData.scope;
-        const len = scopeData.length;
+        var count = 5;
+        var mapdata = {
+            branch : 'CSE',
+            batch : '2017',
+            scope : ['cc','echo','ab', 'bc','abcd','bvv', 'fh','mk','klk','jjj']
+        };
         var resData = [];
-        var limit = 50;
-        var objsData;
-        if(len<9){
-            scopeData.push('global');
-            objsData = await postsRef.where('scope','array-contains-any', scopeData).orderBy('timeStamp','asc').limit(50).get();
-        }
-        else{
-            let newScope = scopeData.slice(0,9);
-            if(len-9 >= 0)
-            newScope.push('global');
-            objsData = await postsRef.where('scope','array-contains-any', newScope).orderBy('timeStamp', 'asc').limit(50).get();
-        }
-        objsData.forEach(element => {
-            console.log(element.id);
-            console.log(element.data());
+        const postsRef = await db.collection('posts').orderBy('timeStamp', 'asc').limit(10).get();
+        postsRef.forEach(element => {
+            var data = element.data();
+            if(typeof data.scope === 'boolean'){
+                data.postId = element.id;
+                data.likes = data.likes.length;
+                data.comments = data.comments.length;
+                resData.push(data);
+            }
+            else if(typeof data.scope === "object"){
+                if(typeof data.scope.batch === 'object'){
+                    if(data.scope.batch.includes(mapdata.batch)){
+                        //
+                    }
+                }
+                //else
+            }
         });
-        return res.status(200).send('All the post data fetched');
+        console.log(resData);
+        return res.status(200).send('done');
     } catch (err) {
         return res.send(err.toString());
     }
