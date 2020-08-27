a086b97997c31f0f0acef13d28bd5bcbe750bcdd
rustiever <sharanzoff@gmail.com>
2020-08-27 18:01:19 +0530

    done nothing

--- functions/faculty/routes/home.js
+++ functions/faculty/routes/home.js
@@ -3,8 +3,9 @@ const firebase = require("firebase");
 
 const db = require("../../auth/app");
 const middleware = require("../auth/middleware");
-
+// need to check edge cases like end of the documents
 homeRoute.get("/", async (req, res, next) => {
+  const limit = 5;
   try {
     // const docData = await (await db.collection(req.usertype).doc(req.uid).get()).data();
     const docData = await (
@@ -30,17 +31,36 @@ homeRoute.get("/", async (req, res, next) => {
       ],
     };
     var resData = [];
-    let time = null ;//time = req.body.timeval;
+    let time = null;//time = req.body.timeval;
+    
     while (con) {
-      console.log(resData.length);
-      if(resData.length>5)break; 
+      console.log(resData.length.toString()+'arr len');
       let postsRef;
       // eslint-disable-next-line no-await-in-loop
-      if(!time)postsRef = await db.collection("posts").orderBy("timeStamp", 'desc').limit(2).get();
+      if (!time) { 
+        postsRef = await db.collection("posts").orderBy("timeStamp", 'desc').limit(limit).get(); 
+        
+        console.log(postsRef.docs.length.toString() + 'how');
+      }
       // eslint-disable-next-line no-await-in-loop
-      else postsRef = await db.collection("posts").orderBy("timeStamp", 'desc').startAfter(time).limit(2).get();
-      let last = postsRef.docs[postsRef.docs.length-1];
+      else { postsRef = await db.collection("posts").orderBy("timeStamp", 'desc').startAfter(time).limit(limit).get(); 
+      console.log(postsRef.docs.length.toString() + 'st' );
+    
+    }
+      if (resData.length >= limit) 
+       {
+        console.log('breaking the while loop');
+        break;
+      }
+      
+      // if( postsRef.docs.length < limit) {
+      //   console.log('why 90');
+      //   break;
+      // };
+
+      let last = postsRef.docs[postsRef.docs.length - 1];
       time = last.data().timeStamp;
+      console.log(time);
       postsRef.forEach(element => {
         let data = element.data();
         if (typeof data.scope === "boolean" && data.scope === false) {
@@ -49,7 +69,7 @@ homeRoute.get("/", async (req, res, next) => {
           data.comments = data.comments.length;
           resData.push(data);
         }
-        else{
+        else {
           if (data.scope.groups === false && (req.body.usertype === 'faculty' || (data.scope.batch === true || (typeof data.scope.batch === "object" && data.scope.batch.includes(qualifier.batch))))) {
 
             if (data.scope.branch === true || (typeof data.scope.branch === "object" && data.scope.branch.includes(qualifier.branch))) {
@@ -68,7 +88,7 @@ homeRoute.get("/", async (req, res, next) => {
           }
         }
       });
-      console.log(resData)
+      // console.log(resData)
     }
     return res.status(200).send(resData);
 
@@ -86,69 +106,7 @@ homeRoute.get("/", async (req, res, next) => {
 
 
 
-    // eslint-disable-next-line no-await-in-loop
-    // const postsRef = await db.collection("posts").orderBy("timeStamp", 'desc').limit(20).get();
-    // postsRef.forEach((element) => {
-    //   var data = element.data();
 
-    //   if (typeof data.scope === "boolean" && data.scope === false) {
-    //     console.log('came here');
-    //     data.postId = element.id;
-    //     data.likes = data.likes.length;
-    //     data.comments = data.comments.length;
-    //     resData.push(data);
-    //   } else {
-    //     if (data.scope.groups === false && (req.body.usertype === 'faculty' || (data.scope.batch === true || (typeof data.scope.batch === "object" && data.scope.batch.includes(qualifier.batch))))) {
-
-    //       if (data.scope.branch === true || (typeof data.scope.branch === "object" && data.scope.branch.includes(qualifier.branch))) {
-    //         data.postId = element.id;
-    //         data.likes = data.likes.length;
-    //         data.comments = data.comments.length;
-    //         resData.push(data);
-    //       }
-    //     } else if (data.scope.branch === false && data.scope.batch === false && typeof data.scope.groups === "object") {
-    //       if (data.scope.groups.some((v) => qualifier.groups.indexOf(v) !== -1)) {
-    //         data.postId = element.id;
-    //         data.likes = data.likes.length;
-    //         data.comments = data.comments.length;
-    //         resData.push(data);
-    //       }
-    //     }
-    //   }
-    // });
-
-    // var qualifier = {
-    //   branch: "CS",
-    //   batch: 2016,
-    //   groups: [
-    //     "cc",
-    //     "echo",
-    //     "ab",
-    //     "bc",
-    //     "abcd",
-    //     "bvv",
-    //     "fh",
-    //     "mk",
-    //     "klk",
-    //     "jjj",
-    //   ],
-    // };
-    // var resData = [];
-
-    // var count = 0;
-    // const con = true;
-    // while(con){
-    //   if(count > 2 ){
-    //     return res.send(resData);
-    //   }
-    //   // eslint-disable-next-line no-await-in-loop
-    //   const postsRef = await db.collection("posts").orderBy("timeStamp", 'desc').limit(20).get();
-    //   postsRef.forEach(element => {
-    //     resData.push(element.data());
-    //   });
-    //   count += 1;
-
-    // }
   } catch (err) {
     return res.send(err.toString());
   }
