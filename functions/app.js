e11ad5d6c4af9dcc3684fce183005693bacd7f62
Sha-Kumar <shashankneeded@gmail.com>
2020-08-29 17:27:55 +0530

    structure correction

--- functions/app.js
+++
@@ -1,7 +0,0 @@
-const firebase = require('firebase');
-
-const firebaseConfig = require('./auth/config');
-
-firebase.initializeApp(firebaseConfig.firebaseConfig);
-
-module.exports = firebase.firestore();
